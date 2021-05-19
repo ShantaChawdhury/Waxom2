dae6ca3232d0212c9ca351acadaefe412f911330
Shanta Chawdhury <84470108+ShantaChawdhury@users.noreply.github.com>
2021-05-20 00:05:41 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,77 @@
+$(function(){
+
+
+
+$(window).scroll(function(){
+    var scrolling = $(this).scrollTop();
+    if(scrolling >200){
+    $('.navbar').addClass('bg');
+    }
+    else{
+    $('.navbar').removeClass('bg');
+    }
+
+    });
+
+
+    var containerEl = document.querySelector('.rim');
+
+    var mixer = mixitup(containerEl);
+
+
+    $(document).ready(function(){
+        $('.venobox').venobox(); 
+    });
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+    
+$('.slide_text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+        speed:1500,
+  });
+// icon slider 
+//   $('.slide_rim').slick({
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     arrows: false,
+//         speed:1500,
+//   });
+
+
+  $('.s_rim').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    speed:1500,
+
+
+    responsive: [
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+          }
+        },
+      
+       
+       
+      ]
+  });
+
+
+});
